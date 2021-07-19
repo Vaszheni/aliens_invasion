5a8c8478d8c8bb14549f1a117f6a5964d2119b7e
Vaszheni <Diox@ukr.net>
2021-07-19 21:07:43 +0300

    first commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 
+from settings import Settings
+
 
 class AlienInvasion:
     """Класс для управления ресурсами и воведением игры"""
@@ -8,9 +10,13 @@ class AlienInvasion:
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
         pygame.display.set_caption("Alien Invasion")
+
 
     def run_game(self):
         """Запуск основного цикла игры"""
@@ -19,10 +25,15 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            #При каждом проходе перерисовываем цикл
+            self.screen.fill(self.settings.bg_color)
+
             # отображение последнегого прорисованого экрана
             pygame.display.flip()
 
+
 if __name__ == "__main__":
-    #Создание екземпляра создания игры
-    ai = AlienInvasion
+    # Создание екземпляра создания игры
+    ai = AlienInvasion()
     ai.run_game()
